f2c16d6d551be738d2c832ca78d6366d74f6f233
tinywell <zfh-zyw@163.com>
2019-06-05 11:27:35 +0800

    handler support concurrent pool

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -7,16 +7,16 @@ import (
 )
 
 // Handler client handler for transaction
-type Handler interface {
-	HandleMessage(ctx context.Context, msg Message)
-	RegisterEvent() error
-	GetEvent() <-chan sdk.Event
-	RegisterTxHandleFunc(trancode TranCode, handleFunc TxHandleFunc) error
-	RegisterSrcHandlerFunc(trancode TranCode, handleFunc SrcHandleFunc) error
-}
+// type Handler interface {
+// 	HandleMessage(ctx context.Context, msg Message)
+// 	RegisterEvent() error
+// 	GetEvent() <-chan sdk.Event
+// 	RegisterTxHandleFunc(trancode TranCode, handleFunc TxHandleFunc) error
+// 	RegisterSrcHandlerFunc(trancode TranCode, handleFunc SrcHandleFunc) error
+// }
 
 // TxHandleFunc transaction handle function
-type TxHandleFunc func(msg Message, handler sdk.TxHandler)
+type TxHandleFunc func(ctx context.Context, msg Message, handler sdk.TxHandler)
 
 // SrcHandleFunc resource management handle function
-type SrcHandleFunc func(msg Message, handler sdk.ResourceManager)
+type SrcHandleFunc func(ctx context.Context, msg Message, handler sdk.ResourceManager)
--- pkg/handler/handlerimpl.go
+++ pkg/handler/handlerimpl.go
@@ -3,8 +3,16 @@ package handler
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/tinywell/fabclient/pkg/sdk"
+)
+
+var (
+	defaultParams = Params{
+		TxTimeout: 30,
+		PoolSize:  200,
+	}
 )
 
 // Impl implement for handler
@@ -17,6 +25,8 @@ type Impl struct {
 	srcHandlers   map[TranCode]SrcHandleFunc
 	tranCodeStore map[TranCode]TranType
 	doneC         chan struct{}
+	timeOut       time.Duration
+	pool          chan struct{}
 	// logger
 }
 
@@ -27,16 +37,31 @@ type Core struct {
 	// ...
 }
 
+// Params handler params
+type Params struct {
+	TxTimeout uint32 // Second
+	PoolSize  uint32 // Concurrent handler size
+}
+
 // NewHandler return new Impl
-func NewHandler(core Core) (*Impl, error) {
+func NewHandler(core Core, param Params) (*Impl, error) {
+	if param.PoolSize == 0 {
+		param.PoolSize = defaultParams.PoolSize
+	}
+	if param.TxTimeout == 0 {
+		param.TxTimeout = defaultParams.TxTimeout
+	}
+
 	h := &Impl{
-		msgs:          make(chan Message, 100),
+		msgs:          make(chan Message),
 		txHandler:     core.TxHandler,
 		txHandlers:    make(map[TranCode]TxHandleFunc),
 		srcHandler:    core.SrcManager,
 		srcHandlers:   make(map[TranCode]SrcHandleFunc),
 		tranCodeStore: make(map[TranCode]TranType),
 		doneC:         make(chan struct{}),
+		timeOut:       time.Second * time.Duration(param.TxTimeout),
+		pool:          make(chan struct{}, param.PoolSize),
 	}
 	go h.server()
 	return h, nil
@@ -46,38 +71,47 @@ func (h *Impl) server() {
 	for {
 		select {
 		case msg := <-h.msgs:
-			trancode := msg.TranCode
-			handlerType, ok := h.tranCodeStore[trancode]
-			if !ok {
-				// trancode not support,return error msg
-				continue
-			}
-
-			switch handlerType {
-			case TypeTxHandler:
-				if h.txHandler == nil {
-					panic(fmt.Errorf("TxHandler not set"))
-				}
-				hfunc, ok := h.txHandlers[trancode]
-				if !ok {
-					// should not hanppen
-
-				}
-				hfunc(msg, h.txHandler)
-			case TypeSrcHandler:
-				if h.txHandler == nil {
-					panic(fmt.Errorf("SrcManager not set"))
-				}
-				hfunc, ok := h.srcHandlers[trancode]
-				if !ok {
-					// should not hanppen
-
-				}
-				hfunc(msg, h.srcHandler)
-			}
+			h.pool <- struct{}{}
+			go h.handler(msg)
 		case <-h.doneC:
 			return
 		}
+	}
+}
+
+func (h *Impl) handler(msg Message) {
+	defer func() {
+		<-h.pool
+	}()
+	trancode := msg.TranCode
+	handlerType, ok := h.tranCodeStore[trancode]
+	if !ok {
+		// trancode not support,return error msg
+		return
+	}
+	ctx, cancelFunc := context.WithTimeout(context.Background(), h.timeOut)
+	defer cancelFunc()
+	switch handlerType {
+	case TypeTxHandler:
+		if h.txHandler == nil {
+			panic(fmt.Errorf("TxHandler not set"))
+		}
+		hfunc, ok := h.txHandlers[trancode]
+		if !ok {
+			// should not hanppen
+			return
+		}
+		hfunc(ctx, msg, h.txHandler)
+	case TypeSrcHandler:
+		if h.srcHandler == nil {
+			panic(fmt.Errorf("SrcManager not set"))
+		}
+		hfunc, ok := h.srcHandlers[trancode]
+		if !ok {
+			// should not hanppen
+			return
+		}
+		hfunc(ctx, msg, h.srcHandler)
 	}
 }
 
@@ -92,7 +126,10 @@ func (h *Impl) HandleMessage(ctx context.Context, msg Message) {
 
 // RegisterEvent register fabric event, event messges will be boradcast throw channel h.events
 func (h *Impl) RegisterEvent(channel, ccName string, event string) error {
-	eventChan := h.txHandler.RegisterEvent(channel, ccName, event)
+	eventChan, err := h.txHandler.RegisterEvent(channel, ccName, event)
+	if err != nil {
+		return err
+	}
 	go func(event <-chan *sdk.Event) {
 		for e := range event {
 			h.events <- *e
